798dc8475581356750a30e358a5d90a241003476
mikael <mikael@DESKTOP-T9O3CIJ>
2016-09-13 19:56:23 +0200

    Initial Commit

---
+++ src/AzureCompetence1/Controllers/ImagesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace AzureCompetence1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ImagesController : Controller
+    {        
+        [HttpGet()]
+        public string[] Get()
+        {
+            return new[] {"Nisse", "Kalle"};
+        }
+     
+        [HttpGet("{id}")]
+        public HttpResponseMessage Get(string imageName)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+        }         
+    }
+}
